70bc354802a88fec7133fe50cf69a6fe28152ae7
Ryan Fowler <ryan.fowler@singlewire.com>
2014-05-19 17:02:07 -0500

    A cluster of 3 comes up in coreos/vagrant

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN pip install python-etcd
 RUN curl -O "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.zip"
 RUN unzip elasticsearch-1.1.1.zip
 RUN mv elasticsearch-1.1.1 elasticsearch
-ADD ./elasticsearch.yml.template /elasticsearch/conf/elasticsearch.yml.template
+ADD ./elasticsearch.yml.template /elasticsearch/config/elasticsearch.yml.template
 ADD ./elasticsearch.py /elasticsearch.py
 CMD ["/elasticsearch.py"]
--- elasticsearch.py
+++ elasticsearch.py
@@ -2,19 +2,43 @@
 from string import Template
 import socket
 import os
+import subprocess
 import etcd
 import json
+import time
+import signal
+import string
 def go():
-    print("Starting Elasticsearch")
-    f = open('/elasticsearch/conf/elasticsearch.yml.template', 'r')
+    f = open('/elasticsearch/config/elasticsearch.yml.template', 'r')
     s = Template(f.read())
     f.close()
-
-    f = open('/elasticsearch/conf/elasticsearch.yml', 'w')
     submap = os.environ.copy()
+    public_ip = socket.gethostbyname(submap['COREOS_PUBLIC_IPV4'])
+    client = etcd.Client(host=submap['COREOS_PUBLIC_IPV4'])
+    submap['CLUSTER_NAME'] = 'elasticsearch'
+    # stick this node in the elasticsearch nodelist
+    r = client.write("/services/elasticsearch", public_ip, append=True, ttl=30)
+    es_node = r.value
+    time.sleep(1)
+    es_all_nodes = client.get("/services/elasticsearch")
+    # lets consider the first elastsearch node a master
+    masta = es_all_nodes.children.next().value
+
+    submap['DISCOVERY_HOSTS'] = str([str(n.value) for n in es_all_nodes.children])
+    submap['PUBLIC_IP'] = public_ip
+    f = open('/elasticsearch/config/elasticsearch.yml', 'w')
+
     f.write(s.substitute(submap))
     f.close()
-    os.system("/elasticsearch/bin/elasticsearch")
+    proc = subprocess.Popen(["/elasticsearch/bin/elasticsearch"])
+
+    signal.signal(signal.SIGINT, lambda s, f: proc.send_signal(signal.SIGINT))
+    signal.signal(signal.SIGTERM, lambda s, f: proc.send_signal(signal.SIGTERM))
+
+    while not proc.returncode:
+      es_node = client.write(r.key, public_ip, append=False, ttl=30).value
+      time.sleep(20)
+      proc.poll()
 
 if __name__ == '__main__':
     go()
